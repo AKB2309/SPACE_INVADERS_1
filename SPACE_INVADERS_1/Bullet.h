b97e8cf195ce3061da13ebaebcc5125ba6d122e5
Boris <borisstyordanov@gmail.com>
2025-06-05 12:22:25 +0300

    bullet.h

--- SPACE_INVADERS_1/Bullet.h
+++ SPACE_INVADERS_1/Bullet.h
@@ -1,5 +1,22 @@
 #pragma once
+#include "GameObject.h"
 class Bullet
 {
+protected:
+	int direction;
+public:
+	Bullet();
+	Bullet(const Bullet& obj);
+	Bullet(Bullet&& obj);
+
+	~Bullet();
+
+	GameObject& operator=(const GameObject& obj);
+
+	int getDirection()const;
+	void setDirection(int d);
+
+	void update();
+	void render() const;
 };
 
