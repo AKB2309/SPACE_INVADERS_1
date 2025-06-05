1abdbe0c356f5d319c5277c47b63091b23e6096a
Boris <borisstyordanov@gmail.com>
2025-06-05 11:19:47 +0300

    GameObject

--- SPACE_INVADERS_1/GameObject.h
+++ SPACE_INVADERS_1/GameObject.h
@@ -1,5 +1,35 @@
 #pragma once
+#include "visualization.h"
 class GameObject
 {
+protected:
+	int x, y;
+	char symbol;
+	COLORS color;
+
+public:
+	GameObject();
+	GameObject(const GameObject& obj);
+	GameObject(GameObject&& obj);
+	virtual ~GameObject();
+
+	GameObject& operator=(const GameObject& obj);
+
+	int getX() const;
+	void setX(int newX);
+
+	int getY() const;
+	void setY(int newY);
+
+	char getSymbol() const;
+	void setSymbol(char newSymbol);
+
+	COLORS getColor() const;
+	void setColor(COLORS newColor);
+
+	virtual void update();
+	virtual void render() const;
+
+
 };
 
