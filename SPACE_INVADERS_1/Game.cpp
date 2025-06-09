e81613e78072c3b039a5c29ad4d51fc803c2f56f
Tedy <Tedy@DESKTOP-0ECB541>
2025-06-09 11:24:40 +0300

    types of enemies change

--- SPACE_INVADERS_1/Game.cpp
+++ SPACE_INVADERS_1/Game.cpp
@@ -36,10 +36,10 @@ void Game::initializeEnemies()
 {
 	for (int i = 0; i < 5; i++)
 	{
-		enemies.push_back(new EnemyType1(5 + i * 6, 2));
-		enemies.push_back(new EnemyType2(5 + i * 6, 4));
-		enemies.push_back(new EnemyType3(5 + i * 6, 6));
-		enemies.push_back(new EnemyType4(5 + i * 6, 8));
+		enemies.push_back(new EnemyType4(5 + i * 6, 2));
+		enemies.push_back(new EnemyType3(5 + i * 6, 4));
+		enemies.push_back(new EnemyType2(5 + i * 6, 6));
+		enemies.push_back(new EnemyType1(5 + i * 6, 8));
 	}
 }
 
